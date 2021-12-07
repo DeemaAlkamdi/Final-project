c8b71159ed2f488ad0f16c251c814ee11a96a7a4
dmdm <deemamoh15@hotmail.com>
2021-12-07 11:16:11 +0300

    I finished a quarter of project

---
+++ aSPA/aSPA/Views/PhotosCell.swift
@@ -0,0 +1,13 @@
+//
+//  PhotosCellVC.swift
+//  aSPA
+//
+//  Created by dmdm on 01/12/2021.
+//
+
+import UIKit
+
+class PhotosCell: UICollectionViewCell {
+    
+    @IBOutlet weak var imgNalis: UIImageView!
+}
